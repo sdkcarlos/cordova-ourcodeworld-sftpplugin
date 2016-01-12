43f8c56af146aa568d17bc1246b5d40e96b65371
Carlos Delgado <sdkcarlos@outlook.com>
2016-01-12 17:18:08 +0100

    Update

--- src/android/OurCodeWorldSFTP.java
+++ src/android/OurCodeWorldSFTP.java
@@ -11,10 +11,61 @@ public class OurCodeWorldSFTP extends CordovaPlugin {
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("connect")) {
-
-            String name = data.getString(0);
-            String message = "SFTP CONNECTED:			" + name;
-            callbackContext.success(message);
+			 
+			try {
+				JSONObject arg_object = args.getJSONObject(0);
+				
+				String hostname = arg_object.getString("host");
+				String login =  arg_object.getString("username");
+				String password =  arg_object.getString("password");
+				String directory =  arg_object.getString("path");
+		 
+				java.util.Properties config = new java.util.Properties();
+				config.put("StrictHostKeyChecking", "no");
+		 
+				JSch ssh = new JSch();
+				Session session = ssh.getSession(login, hostname, 22);
+				session.setConfig(config);
+				session.setPassword(password);
+				session.connect();
+				Channel channel = session.openChannel("sftp");
+				channel.connect();
+		 
+				ChannelSftp sftp = (ChannelSftp) channel;
+				sftp.cd(directory);
+				Vector files = sftp.ls("*");
+				//System.out.printf("Found %d files in dir %s%n", files.size(), directory);
+		 
+				for (ChannelSftp.LsEntry file : files) {
+					if (file.getAttrs().isDir()) {
+						continue;
+					}
+					//System.out.printf("Reading file : %s%n", file.getFilename());
+					BufferedReader bis = new BufferedReader(new InputStreamReader(sftp.get(file.getFilename())));
+					String line = null;
+					while ((line = bis.readLine()) != null) {
+						//System.out.println(line);
+						String name = line.getString(0);
+						String message = name;
+						callbackContext.success(message);
+					}
+					bis.close();
+				}
+		 
+				channel.disconnect();
+				session.disconnect();
+			} catch (JSchException e) {
+				e.printStackTrace();  
+			} catch (SftpException e) {
+				e.printStackTrace();
+			}
+			
+			
+			
+			
+            //String name = data.getString(0);
+            //String message = "SFTP CONNECTED:			" + name;
+            //callbackContext.success(message);
 
             return true;
 
--- www/ourcodeworldsftp.js
+++ www/ourcodeworldsftp.js
@@ -1,7 +1,41 @@
 /*global cordova, module*/
 
 module.exports = {
+	_settings:{
+		host:null,
+		username:null,
+		password:null,
+		path:'/root'
+	},
+	setCredentials: function(host,username,password){
+		if(typeof(host) === "undefined"){
+			this._settings.host = null;
+		}else{
+			this._settings.host = host;	
+		}
+		
+		if(typeof(username) === "undefined"){
+			this._settings.username = null;
+		}else{
+			this._settings.username = username;	
+		}
+		
+		if(typeof(password) === "undefined"){
+			this._settings.password = null;
+		}else{
+			this._settings.password = password;	
+		}
+	},
+	setPath: function(path){
+		if(typeof(path) === "undefined"){
+			this._settings.path = null;
+		}else{
+			this._settings.path = '/root';	
+		}
+	},
     connect: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "OurCodeWorldSFTP", "connect", [name]);
+		var datos = this._settings;
+		cordova.exec(successCallback, errorCallback, "OurCodeWorldSFTP", "connect", [datos]);
+        //cordova.exec(successCallback, errorCallback, "OurCodeWorldSFTP", "connect", [name]);
     }
 };
