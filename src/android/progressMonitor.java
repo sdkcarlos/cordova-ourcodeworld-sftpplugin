b157fbcb6ea2a77158d2ac410186edfecab93f3c
Carlos Delgado <sdkcarlos@outlook.com>
2016-01-19 17:21:35 +0100

    Progress Big Update

--- src/android/progressMonitor.java
+++ src/android/progressMonitor.java
@@ -7,7 +7,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class progressMonitor implements SftpProgressMonitor{
-    
     private long max                = 0;
     private long count              = 0;
     private long percent            = 0;
@@ -20,9 +19,18 @@ public class progressMonitor implements SftpProgressMonitor{
 
     public void init(int op, java.lang.String src, java.lang.String dest, long max) {
         this.max = max;
-        //Log.d("SFTP Plugin - JJDLTC", "Action Start, From: "+src+" <=> To: "+dest);
-        ///Log.d("SFTP Plugin - JJDLTC", "Action Start, Size: "+max+" <=> Opt: "+op);
-        //this.jsEvent("SFTPActionStart", "{from:'"+src+"',to:'"+dest+"',size:'"+max+"'}");
+        try{
+            JSONObject item = new JSONObject();
+            item.put("started",true);
+            item.put("from",src);
+            item.put("filesize",max);
+            PluginResult result = new PluginResult(PluginResult.Status.OK, item.toString());
+            result.setKeepCallback(true);
+            callbacks.sendPluginResult(result);
+        } catch (JSONException e) {
+            callbacks.error(e.getMessage().toString());
+            e.printStackTrace();
+        }
     }
 
     public boolean count(long bytes){
@@ -30,8 +38,6 @@ public class progressMonitor implements SftpProgressMonitor{
         long percentNow = this.count*100/max;
         if(percentNow>this.percent){
             this.percent = percentNow;
-            //this.jsEvent("SFTPActionProgress", "{percent:'"+percent+"'}");
-           // Log.d("SFTP Plugin - JJDLTC", "Action Progress: "+this.percent+"%");
             try{
                 JSONObject item = new JSONObject();
                 item.put("finished",false);
